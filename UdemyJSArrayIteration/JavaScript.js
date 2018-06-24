a02d9b1ace9b6fece00b9a71c78c912446f340b1
Amy Thompson <amythompson9@gmail.com>
2018-06-23 22:03:18 -0400

    Quiz

--- UdemyJSArrayIteration/JavaScript.js
+++ UdemyJSArrayIteration/JavaScript.js
@@ -1,1 +1,10 @@
-﻿
+﻿var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var colors = ["red", "orange", "yellow", "green"];
+
+number.forEach(funtion(color) {
+    if(color % 3 === 0) {
+        console.log(color);
+    }
+});
+
+//should print 3, 6, 9
